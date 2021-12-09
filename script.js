b127fbce91087abc5c353f1bf10744483c1bc96d
Yousef Sayed <sayed.yousaf@gmail.com>
2021-12-09 10:05:20 +0000

    add click listener to cells

--- script.js
+++ script.js
@@ -20,19 +20,32 @@ const gameModel = (() => {
 })();
 
 const gameView = (() => {
-  const boardView = document.querySelector(".board");
-  const displayBoard = (board) => {
-    let boardViewContent = "";
-    board.forEach((row, rowIndex) => {
-      boardViewContent += '<div class="row">';
-      row.forEach(
-        (cell, cellIndex) =>
-          (boardViewContent += `<div class="cell" data-row=${rowIndex} data-column=${cellIndex}>${cell}</div>`)
-      );
+  const createRow=()=> {
+    const row=document.createElement('div')
+    row.classList.add('row')
+    return row
+  }
 
-      boardViewContent += "</div>";
+  const createCell=(cellContent)=>{
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    cell.innerText=cellContent
+    return cell;
+  }
+
+  const addCell =(row,cell)=>{
+        cell.addEventListener('click',()=> cell.innerText='X')
+        row.appendChild(cell)
+  }
+
+  const boardView = document.querySelector(".board");
+
+  const displayBoard = (board) => {
+    board.forEach((boradRow) => {
+      const row = createRow()
+      boradRow.forEach( (cellContent) =>addCell(row,createCell(cellContent)) );
+    boardView.appendChild(row)
     });
-    boardView.innerHTML = boardViewContent;
   };
   return { displayBoard };
 })();
