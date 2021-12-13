cc052c29c893afc634d9ef0b9e344a95e41488e3
Yousef Sayed <sayed.yousaf@gmail.com>
2021-12-13 09:24:35 +0000

    move controller logic to its own component

--- src/index.js
+++ src/index.js
@@ -1,84 +1,24 @@
-//create gameboard object
-//create player object
-//store player array inside gameboard object
-//create game controller object
-//least possible global code
-//use module pattern or factory pattern
-//factory pattern is a function that returns a closure as an object
-//module pattern is a factory in the form of ieef
-//Use module pattern if you a singleton is required
-//Use factory pattern if multiple objects of a type is required
-import view from "./components/view";
-import model from "./components/model";
-import player from "./components/player";
-import turn from "./components/turn";
-
+// move related parts to veiw.js
+// get input values and assign them to players
 import "./index.html";
 import "./styles/index.scss";
+import controller from "./components/controller";
+const display = document.querySelector(".display");
+const start = document.querySelector(".start");
+const board = document.querySelector(".board");
+let xPlayer = "X";
+let oPlayer = "O";
 
-const gameController = (() => {
-  const board = model.board;
+controller.stateListers.push((oldState, newState) => {
+  display.innerHTML = `oldState: ${oldState} newState: ${newState}`;
+  board.classList.add("board-disabled");
+  board.classList.add("text-muted");
+});
 
-  const winnerCheck = () => {
-    //rowcheck
-    for (let i = 0; i < 3; i++) {
-      const row = board[i];
-      const sum = row.reduce((accumlator, curr) => accumlator + curr);
-      if (sum === 3 || sum === -3) return sum;
-    }
-
-    //colcheck
-    for (let colIndex = 0; colIndex < 3; colIndex++) {
-      let sum = 0;
-      for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
-        sum += board[rowIndex][colIndex];
-      }
-      if (sum === 3 || sum === -3) return sum;
-    }
-
-    // diagonal check
-    let sum = board[0][0] + board[1][1] + board[2][2];
-    if (sum === 3 || sum === -3) return sum;
-    sum = board[0][2] + board[1][1] + board[2][0];
-    if (sum === 3 || sum === -3) return sum;
-
-    // tie check
-    return board.reduce(
-      (accumlator, currentArray) =>
-        accumlator && currentArray.reduce((acc, curr) => acc && curr)
-    );
-  };
-
-  const O = -1;
-  const X = 1;
-  const Owins = -3;
-  const Xwins = 3;
-  const Tie = 1;
-
-  const players = [player(O), player(X)];
-
-  view.createView(board, (coordinates) => {
-    const currnetCell = model.getCell(coordinates);
-
-    if (!currnetCell) {
-      const marker = players[turn.next()].side;
-      model.setCell(coordinates, marker);
-    }
-
-    const result = winnerCheck();
-
-    switch (result) {
-      case Owins:
-        console.log("O is the WINNER!");
-        break;
-      case Xwins:
-        console.log("X is the WINNER!");
-        break;
-      case Tie:
-        console.log("It is a TIE");
-        break;
-    }
-
-    view.updateView(board);
-  });
-})();
+start.addEventListener("click", (e) => {
+  controller.clear();
+  board.classList.remove("board-disabled");
+  display.innerHTML = "Good luck!";
+  start.innerHTML = "Restart";
+  e.preventDefault();
+});
